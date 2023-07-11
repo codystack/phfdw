dba42dfbcfac40f5575335e5220861ea92a468ad
ThankGod Okoro <ebuzzadvert@gmail.com>
2023-07-11 10:46:23 +0100

    registration closed

--- showcase.php
+++ showcase.php
@@ -1,12 +1,78 @@
 <?php
-session_start();
-include "./components/header.php";
-include "./components/fixednavbarlight.php";
-include "./components/mobilenavbar.php";
-require_once "./controller/auth.php";
+// session_start();
+// include "./components/header.php";
+// include "./components/fixednavbarlight.php";
+// include "./components/mobilenavbar.php";
+// require_once "./controller/auth.php";
 ?>
 
+
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>404 | PHFDW&trade;</title>
+        <meta name="author" content="ThankGod Okoro">
+        <meta property="og:url" content="https://phfashionweek.com/"/>
+	    <meta property="og:type" content="website" />
+        <meta property="og:title" content="TG&trade; :: Web + App Developer, UI/UX Designer, Product Designer & Graphic Designer." />
+        <meta name="og:description" content="The Port Harcourt Fashion and Design Week is a three-day fashion event targeted at promoting sustainable development through stimulating youths who have interest and potentials in the fashion industry, with the necessary tools that will enable, encourage and support their aspirations of being successful and economically empowered youths.">
+        <meta name="keywords" content="Port Harcourt,PH,Pitakwa,PHFDW,Fashion,Port Harcourt Fashion & Design Week,Design,Week,Bole,Fish,Oil and Gas,Slay,Velisco">
+        <link rel="icon" type="image/png" href="assets/images/favicon.png">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&amp;display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="assets/vendor/swiper/dist/css/swiper.min.css" />
+        <link rel="stylesheet" href="assets/vendor/fancybox/dist/jquery.fancybox.min.css" />
+        <link rel="stylesheet" href="assets/css/style.min.css">
+        <link rel="stylesheet" href="assets/css/custom.css">
+        <script src="assets/vendor/jquery/dist/jquery.min.js"></script>
+    </head>
+
+<body>
+
         <div class="content-wrap">
+            <div class="py-160 text-center">
+                <div class="container shape-parent">
+                    <div class="shape mt-n160 ml-30">
+                        <svg class="mt-n160" width="500" height="318" viewBox="0 0 500 318" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <circle cx="250" cy="68" r="250" fill="#F5F5F5" />
+                        </svg>
+                    </div>
+                    <div class="lines-style-6">
+                        <div class="line show-on-scroll" data-show-duration="500" data-show-distance="140" data-show-delay="400" data-show-origin="right"></div>
+                        <div class="line show-on-scroll" data-show-duration="500" data-show-distance="120" data-show-delay="200"></div>
+                    </div>
+                    <!-- <h1 class="display-3 mb-12 mt-2 show-on-scroll" data-show-duration="600" data-show-distance="10">404</h1> -->
+                    <img src="./assets/images/do-not-enter.png" class="mb-10" width="200px" alt="">
+                    <p class="h3 alert-link mb-10 show-on-scroll mt-15" data-show-duration="500" data-show-distance="10" data-show-delay="100">Registration Closed 😿</p>
+                    <p class="mb-40 pb-2 show-on-scroll" data-show-duration="500" data-show-distance="10" data-show-delay="200">Perhaps you would like to go back to the home page?</p>
+                    <a href="./" class="btn btn-dark btn-with-ball mb-15 show-on-scroll" data-show-duration="400" data-show-distance="10" data-show-delay="230">go to home</a>
+                </div>
+            </div>
+        </div>
+
+        <script src="assets/vendor/object-fit-images/dist/ofi.min.js"></script>
+        <script src="assets/vendor/popper.js/dist/umd/popper.min.js"></script>
+        <script src="assets/vendor/bootstrap/dist/js/bootstrap.min.js"></script>
+        <script src="assets/vendor/bootstrap-validator/dist/validator.min.js"></script>
+        <script src="assets/vendor/imagesloaded/imagesloaded.pkgd.min.js"></script>
+        <script src="assets/vendor/swiper/dist/js/swiper.min.js"></script>
+        <script src="assets/vendor/animejs/lib/anime.min.js"></script>
+        <script src="assets/vendor/rellax/rellax.min.js"></script>
+        <script src="assets/vendor/jquery-countdown/dist/jquery.countdown.min.js"></script>
+        <script src="assets/vendor/moment/min/moment.min.js"></script>
+        <script src="assets/vendor/moment-timezone/builds/moment-timezone-with-data.min.js"></script>
+        <script src="assets/vendor/isotope-layout/dist/isotope.pkgd.min.js"></script>
+        <script src="assets/vendor/isotope-packery/packery-mode.pkgd.min.js"></script>
+        <script src="assets/vendor/jarallax/dist/jarallax.min.js"></script>
+        <script src="assets/vendor/jarallax/dist/jarallax-video.min.js"></script>
+        <script src="assets/vendor/fancybox/dist/jquery.fancybox.min.js"></script>
+        <script src="assets/js/main.min.js"></script>
+    </body>
+
+        <!-- <div class="content-wrap">
             <div class="py-160 bg-dark">
                 <div class="container">
                     <div class="row justify-content-center text-white">
@@ -62,7 +128,7 @@ require_once "./controller/auth.php";
             </div>
         </div>
 
-        <!-- Designers Category Modal -->
+        
         <div class="modal modal-fluid fade" id="designersCategory" tabindex="-1" role="dialog" aria-labelledby="modal_1" aria-hidden="true">
             <div class="modal-dialog modal-lg modal-dialog-centered modal-dialog-scrollable" data-bs-backdrop="static" role="document">
                 <div class="modal-content">
@@ -116,6 +182,6 @@ require_once "./controller/auth.php";
                     </div>
                 </div>
             </div>
-        </div>
+        </div> -->
     
-<?php include "./components/footer.php"; ?>
+<?php //include "./components/footer.php"; ?>
