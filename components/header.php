b95839ce6dd0fbc477870a14748f7ab60b63afc2
ThankGod Okoro <ebuzzadvert@gmail.com>
2023-08-03 16:36:48 +0100

    Facebook Pixel added

--- components/header.php
+++ components/header.php
@@ -33,5 +33,22 @@ else
         <link rel="stylesheet" href="assets/css/style.min.css">
         <link rel="stylesheet" href="assets/css/custom.css">
         <script src="assets/vendor/jquery/dist/jquery.min.js"></script>
+        <!-- Meta Pixel Code -->
+            <script>
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '953376762414449');
+            fbq('track', 'PageView');
+            </script>
+            <noscript><img height="1" width="1" style="display:none"
+            src="https://www.facebook.com/tr?id=953376762414449&ev=PageView&noscript=1"
+            /></noscript>
+        <!-- End Meta Pixel Code -->
     </head>
     <body>
