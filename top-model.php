64032483fe0730f77de57095f49e1d8bbf865336
codystack <ebuzzadvert@gmail.com>
2024-06-04 19:05:32 +0100

    top model form

--- top-model.php
+++ top-model.php
@@ -24,11 +24,31 @@ require_once "./controller/auth.php";
                                     </div>
 
                                     <div class="col-12 col-md-6 show-on-scroll" data-show-duration="500" data-show-distance="10" data-show-delay="50">
-                                        <input type="text" name="brandName" required class="form-control form-control-lg form-control-white" placeholder="Brand Name *">
+                                        <input type="number" name="age" required class="form-control form-control-lg form-control-white" placeholder="Age *">
                                     </div>
 
                                     <div class="col-12 col-md-6 show-on-scroll" data-show-duration="500" data-show-distance="10" data-show-delay="!00">
-                                        <input type="text" name="address" required class="form-control form-control-lg form-control-white" placeholder="Address *">
+                                        <input type="text" name="guardianName" class="form-control form-control-lg form-control-white" placeholder="Name of Guardian">
+                                    </div>
+
+                                    <div class="col-12 col-md-6 show-on-scroll" data-show-duration="500" data-show-distance="10" data-show-delay="!00">
+                                        <input type="tel" name="guardianTel" class="form-control form-control-lg form-control-white" placeholder="Guardian Phone">
+                                    </div>
+
+                                    <div class="col-12 col-md-6 show-on-scroll" data-show-duration="500" data-show-distance="10" data-show-delay="!00">
+                                        <input type="text" name="height" class="form-control form-control-lg form-control-white" placeholder="Height">
+                                    </div>
+
+                                    <div class="col-12 col-md-6 show-on-scroll" data-show-duration="500" data-show-distance="10" data-show-delay="!00">
+                                        <input type="text" name="statistics" class="form-control form-control-lg form-control-white" placeholder="Statistics">
+                                    </div>
+
+                                    <div class="col-12 col-md-6 show-on-scroll" data-show-duration="500" data-show-distance="10" data-show-delay="!00">
+                                        <input type="text" name="shoeSize" class="form-control form-control-lg form-control-white" placeholder="Shoe Size">
+                                    </div>
+
+                                    <div class="col-12 col-md-6 show-on-scroll" data-show-duration="500" data-show-distance="10" data-show-delay="!00">
+                                        <input type="text" name="dressSize" class="form-control form-control-lg form-control-white" placeholder="Dress Size">
                                     </div>
 
                                     <div class="col-12 col-md-6 show-on-scroll" data-show-duration="500" data-show-distance="10" data-show-delay="150">
@@ -40,18 +60,13 @@ require_once "./controller/auth.php";
                                     </div>
 
                                     <div class="col-12 col-md-6 show-on-scroll" data-show-duration="500" data-show-distance="10" data-show-delay="!00">
-                                        <input type="text" name="facebook" class="form-control form-control-lg form-control-white" placeholder="Facebook ID">
-                                    </div>
-
-                                    <div class="col-12 col-md-6 show-on-scroll" data-show-duration="500" data-show-distance="10" data-show-delay="!00">
                                         <input type="text" name="instagram" class="form-control form-control-lg form-control-white" placeholder="Instagram handle">
                                     </div>
 
-                                    <div class="col-12 show-on-scroll" data-show-duration="500" data-show-distance="10" data-show-delay="350">
-                                        Have you participated in a fashion show?
-                                        <textarea type="text" name="participated" class="form-control form-control-lg form-control-white" rows="2" placeholder="If yes give details..."></textarea>
+                                    <div class="col-12 col-md-12 show-on-scroll" data-show-duration="500" data-show-distance="10" data-show-delay="!00">
+                                        <input type="text" name="address" required class="form-control form-control-lg form-control-white" placeholder="Address *">
                                     </div>
-
+                                    
                                 </div>
 
                                 <button class="btn btn-white btn-with-ball mt-90 show-on-scroll" type="submit" name="showcase_btn" data-show-distance="10" data-show-duration="500" data-show-delay="300">submit</button>
